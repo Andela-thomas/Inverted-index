69c6793821d3e6ea97f089248bb4e06fb4978f00
andela-thomas <thomas.nyambati@andela.com>
2015-09-18 12:42:30 +0300

    removed redundant callback in the test

--- spec/inverted-index-test.js
+++ spec/inverted-index-test.js
@@ -1,92 +1,107 @@
-describe('Inverted Index', function() {
-  describe('Read Book Data', function() {
-    describe("Books should not be empty", function() {
-      var hash = new HashTable();
-      function wrapRun(done) {
-        setTimeout(function() {
-          done();
+(function() {
+  'use strict';
+
+  describe('Inverted Index', function() {
+    var hash;
+    describe('Read Book Data', function() {
+      describe('books object should not be empty', function() {
+        beforeEach(function(done) {
+          hash = new HashTable();
+          hash.createIndex('./books.json', done);
         });
-      }
-      beforeEach(function(done) {
-        wrapRun(done);
+
+        it("should support async execution of test preparation and expectations", function() {
+          expect(hash.books.length).toBeGreaterThan(0);
+        });
+
       });
-      it('The Books length should be greater than 0', function() {
-        expect(books.length).not.toBe(0);
+
+      describe('Populate Index', function() {
+        describe('Should Populate the index when the JSON file is read', function() {
+          it('Should return true for Alice', function() {
+            expect(hash.hasItem('alice')).toBe(true);
+          });
+
+          it('Should return true for rabbit ', function() {
+            expect(hash.hasItem('rabbit')).toBe(true);
+          });
+
+          it('Should return true for world ', function() {
+            expect(hash.hasItem('world')).toBe(true);
+          });
+
+          it('Should return false for cow ', function() {
+            expect(hash.hasItem('cow')).toBe(false);
+          });
+
+          it('Should return false for sin ', function() {
+            expect(hash.hasItem('sin')).toBe(false);
+          });
+
+          it('Should return false for engine ', function() {
+            expect(hash.hasItem('engine')).toBe(false);
+          });
+        });
+      });
+
+      describe('Index Maping', function() {
+        describe('Index should map the correct Object', function() {
+          it('should return ["0"] for alice', function() {
+            expect(hash.getItem("alice")).toEqual(['0']);
+          });
+
+          it('should return ["0"] for Alice', function() {
+            expect(hash.getItem('Alice'.toLowerCase())).toEqual(['0']);
+          });
+
+          it('should return "Term not found" for kenya', function() {
+            expect(hash.getItem('kenya')).toEqual("Term not found");
+          });
+
+          it('should return "Term not found" for mosque', function() {
+            expect(hash.getItem('mosque')).toEqual("Term not found");
+          });
+
+          it('should return ["1"] for destroy', function() {
+            expect(hash.getItem('destroy')).toEqual(['1']);
+          });
+
+          it('should return ["1"] Destroy', function() {
+            expect(hash.getItem('Destroy'.toLowerCase())).toEqual(['1']);
+          });
+        });
+      });
+
+      describe('Search Index', function() {
+        describe('Check if search key  has been provided', function() {
+          it("Prompt for search key if it has not been provided", function() {
+            expect(hash.searchIndex()).toEqual('Please provide a search key');
+          });
+        });
+
+        describe('Should return the correct indices for the object', function() {
+
+          it("should return ['1'] for destroy", function() {
+            expect(hash.searchIndex('destroy')).toEqual(['1']);
+          });
+
+          it("should return ['1'],['0'] for destroy hole", function() {
+            expect(hash.searchIndex('destroy hole')).toEqual(['1'], ['0']);
+          });
+
+          it("should return 'No results found ' for mark masai", function() {
+            expect(hash.searchIndex('mark masai')).toEqual("No results found");
+          });
+
+          it("should return ['1'],['1'] for ring man", function() {
+            expect(hash.searchIndex('ring man')).toEqual(['1'], ['1']);
+          });
+
+          it("should return ['0'],['1'] for rabbit hobbit", function() {
+            expect(hash.searchIndex('rabbit hobbit')).toEqual(['0'], ['1']);
+          });
+        });
       });
     });
   });
-
-  describe('Populate Index', function() {
-
-    describe('Should Populate the index when the JSON file is read', function() {
-      it('Should return true for Alice', function() {
-        expect(hash.hasItem('alice')).toBe(true);
-      });
-      it('Should return true for rabbit ', function() {
-        expect(hash.hasItem('rabbit')).toBe(true);
-      });
-      it('Should return true for world ', function() {
-        expect(hash.hasItem('world')).toBe(true);
-      });
-      it('Should return false for cow ', function() {
-        expect(hash.hasItem('cow')).toBe(false);
-      });
-      it('Should return false for sin ', function() {
-        expect(hash.hasItem('sin')).toBe(false);
-      });
-      it('Should return false for engine ', function() {
-        expect(hash.hasItem('engine')).toBe(false);
-      });
-
-    });
-  });
-
-  describe('Index Maping', function() {
-    describe('Index should map the correct Object', function() {
-      it('should return ["0"] for alice', function() {
-        expect(hash.getItem("alice")).toEqual(['0']);
-      });
-      it('should return ["0"] for Alice', function() {
-        expect(hash.getItem('Alice'.toLowerCase())).toEqual(['0']);
-      });
-      it('should return "Term not found" for kenya', function() {
-        expect(hash.getItem('kenya')).toEqual("Term not found");
-      });
-      it('should return "Term not found" for mosque', function() {
-        expect(hash.getItem('mosque')).toEqual("Term not found");
-      });
-      it('should return ["1"] for destroy', function() {
-        expect(hash.getItem('destroy')).toEqual(['1']);
-      });
-      it('should return ["1"] Destroy', function() {
-        expect(hash.getItem('Destroy'.toLowerCase())).toEqual(['1']);
-      });
-    });
-  });
-
-  describe('Search Index', function() {
-    describe('Check if search key  has been provided', function() {
-      it("Prompt for search key if it has not been provided", function() {
-        expect(searchIndex()).toEqual('Please provide a search key');
-      });
-    });
-    describe('Should return the correct indices for the object', function() {
-      it("should return ['1'] for destroy", function() {
-        expect(searchIndex('destroy')).toEqual(['1']);
-      });
-      it("should return ['1'],['0'] for destroy hole", function() {
-        expect(searchIndex('destroy hole')).toEqual(['1'], ['0']);
-      });
-      it("should return 'No results found ' for mark masai", function() {
-        expect(searchIndex('mark masai')).toEqual("No results found");
-      });
-      it("should return ['1'],['1'] for ring man", function() {
-        expect(searchIndex('ring man')).toEqual(['1'], ['1']);
-      });
-      it("should return ['0'],['1'] for rabbit hobbit", function() {
-        expect(searchIndex('rabbit hobbit')).toEqual(['0'], ['1']);
-      });
-    });
-  });
-});
-
+})();
--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,181 +1,170 @@
-  // Thomas Nyambati
-  // Andela ltd
-  // thomas.nyambati@andela.com
-  // Inverted Index
-  // JavaSript Cirriculum check point 1
+// Thomas Nyambati
+// Andela ltd
+// thomas.nyambati@andela.com
+// Inverted Index
+// JavaSript Cirriculum check point 1
 
+var HashTable;
+(function() {
+  'use strict';
+  HashTable = function() {
 
-var HashTable = function() {
-
-   // Defining the hastable class
-   // This table will have the property length
-   // An an object of items to hold the the indexes
-
-  this.length = 0;
-  this.items = {};
-
-  // Method hasItem
-  // finds the key if available
-  this.hasItem = function(key) {
-    return this.items.hasOwnProperty(key);
-    // Return => true || false
+    // Defining the hastable class
+    // This table will have the property length
+    // An an object of items to hold the the indexes
+    this.length = 0;
+    this.items = {};
+    // Method hasItem
+    // finds the key if available
+    this.hasItem = function(key) {
+      return this.items.hasOwnProperty(key);
+      // Return => true || false
+    };
   };
 
-};
+  // Method setItem
+  // This method will set the items into the hastable,
+  // And also ensures that it does not overwrite any key or value.
+  // Checks if the hastable has  the key
+  // Checks if the value passed is also in the hastable
+  // If true push only the index on the file where the term was found
+  // If the term is not in the hashtable
+  // Create a new term with the file and the index where it was found
+  HashTable.prototype.setItem = function(key, index) {
+    var object = this.items[key],
+      exist = false;
 
-// Method setItem
-// This method will set the items into the hastable,
-// And also ensures that it does not overwrite any key or value.
-// Checks if the hastable has  the key
-// Checks if the value passed is also in the hastable
-// If true push only the index on the file where the term was found
-// If the term is not in the hashtable
-// Create a new term with the file and the index where it was found
-HashTable.prototype.setItem = function(key, index) {
-  var object = this.items[key], exist = false;
-
-  for (key in object) {
-    if(object[key] === index) {
-      exist = true;
+    for (key in object) {
+      if (object[key] === index) {
+        exist = true;
+      }
     }
-  }
 
-  if ((this.hasItem(key) && exist === true) === false) {
-    if (this.hasItem(key) && this.items[key] !== index) {
-      this.items[key].push(index);
-    } else {
-      this.items[key] = [index];
-      this.length++;
+    if ((this.hasItem(key) && exist === true) === false) {
+      if (this.hasItem(key) && this.items[key] !== index) {
+        this.items[key].push(index);
+      } else {
+        this.items[key] = [index];
+        this.length++;
+      }
     }
-  }
-};
+  };
 
 
-// Method getItem
-// This method return an array of file and indexex of a given term if found
-// And undefined if not found
-// var index = new hastable();
-// index.getItem('mango') => ['fruits',[0]] or => Term not found
+  // Method getItem
+  // This method return an array of file and indexex of a given term if found
+  // And undefined if not found
+  // var index = new hastable();
+  // index.getItem('mango') => ['fruits',[0]] or => Term not found
 
-HashTable.prototype.getItem = function(key) {
-  return this.hasItem(key) ? this.items[key] : "Term not found";
-};
+  HashTable.prototype.getItem = function(key) {
+    return this.hasItem(key) ? this.items[key] : "Term not found";
+  };
 
-// This function receives an array of all words and returns an
-// array of only keywords excluding conjuctions
-// Checks if the garbage has the element in the consjuctions array
-//
+  // This function receives an array of all words and returns an
+  // array of only keywords excluding conjuctions
+  // Checks if the garbage has the element in the consjuctions array
+  //
 
-HashTable.prototype.getKeyWords = function(garbage) {
-  var conjunctions = ['of', 'the', 'in', 'and', 'an', 'a', 'to', 'into', 'so', ''],
-    filtered = [];
+  HashTable.prototype.getKeyWords = function(garbage) {
+    var conjunctions = ['of', 'the', 'in', 'and', 'an', 'a', 'to', 'into', 'so', ''],
+      filtered = [];
 
-  for (var i = 0; i < garbage.length; i++) {
-    if (conjunctions.indexOf(garbage[i].toLowerCase()) === -1) {
-      filtered.push(garbage[i]);
+    for (var i = 0; i < garbage.length; i++) {
+      if (conjunctions.indexOf(garbage[i].toLowerCase()) === -1) {
+        filtered.push(garbage[i]);
+      }
     }
-  }
 
-  return filtered;
-};
-// This function will fetch the terms and there index reference in the hashtable
+    return filtered;
+  };
+  // This function will fetch the terms and there index reference in the hashtable
 
-HashTable.prototype.getIndex = function() {
-    var values = [], keys;
+  HashTable.prototype.getIndex = function() {
+    var values = [],
+      keys;
     for (keys in this.items) {
-        values.push(this.items[keys]);
+      values.push(this.items[keys]);
     }
     return values;
-};
-
-// This function wil read the Json file
-HashTable.prototype.readFile = function(callback) {
-  var Xmlhttp = new XMLHttpRequest();
-  Xmlhttp.open('GET', url, true);
-  Xmlhttp.setRequestHeader("Content-Type", "application/json");
-
-  Xmlhttp.onreadystatechange = function() {
-    if (Xmlhttp.readyState === 4 && Xmlhttp.status === 200) {
-      var response = Xmlhttp.responseText;
-      callback(response);
-    }
   };
 
-  Xmlhttp.send(null);
-};
+  // This function wil read the Json file
+  HashTable.prototype.readFile = function(url, callback) {
+    var Xmlhttp = new XMLHttpRequest();
+    Xmlhttp.open('GET', url, true);
+    Xmlhttp.setRequestHeader("Content-Type", "application/json");
 
-/*========================================================================================
-  END OF THE HASHTABLE
-=======================================================================================*/
-
-
-// create an instance of our hastable
-var hash = new HashTable();
-
-// This function will populate our hashtable with indexes
-// Combine all the property data into one
-// Filter the data to remove remover special character
-// Filter the data to remove conjuctions such as  => for, as in etc
-// loop through the filtered text to generate index
-
-var createIndex = function(url) {
-  hash.readFile(function(response, url) {
-    books = JSON.parse(response);
-    for (var key in books) {
-      var text = books[key].title.concat(books[key].text);
-      text = text.replace(/[^a-zA-Z ]/g, " ").split(' ');
-      text = hash.getKeyWords(text);
-      for (var i = 0; i < text.length; i++) {
-        hash.setItem(text[i].toLowerCase(), key);
+    Xmlhttp.onreadystatechange = function() {
+      if (Xmlhttp.readyState === 4 && Xmlhttp.status === 200) {
+        var response = Xmlhttp.responseText;
+        callback(response);
       }
-    }
-  });
+    };
 
-};
+    Xmlhttp.send(null);
+  };
 
-// This function will fetch the indexes from our hashtable
-// Check if the words parameter is empty
-// Return undefined if true
-// Split the words to create an array
-// If one words pass it to the getItem function
-// If not loop through the resultant array and pass each element to the getItem function
-var searchIndex = function(words) {
-  var searchKeys, indices = [],
-    term, key, index;
-    console.log(hash.getIndex());
-  if (words !== undefined) {
-    searchKeys = words.split(' ');
-    if (searchKeys.length > 1) {
-      for (key in searchKeys) {
-        term = searchKeys[key];
-        index = hash.getItem(term);
-        if (index !== "Term not found") {
+  // This function will populate our hashtable with indexes
+  // Combine all the property data into one
+  // Filter the data to remove remover special character
+  // Filter the data to remove conjuctions such as  => for, as in etc
+  // loop through the filtered text to generate index
 
-          for (key in index) {
-            indices.push(index[key]);
-          }
-
-        } else {
-          return "No results found";
+  HashTable.prototype.createIndex = function(url, done) {
+    var self = this;
+    this.readFile(url, function(response) {
+      self.books = JSON.parse(response);
+      console.log(self.books);
+      for (var key in self.books) {
+        var text = self.books[key].title.concat(self.books[key].text);
+        text = text.replace(/[^a-zA-Z ]/g, " ").split(' ');
+        text = self.getKeyWords(text);
+        for (var i = 0; i < text.length; i++) {
+          self.setItem(text[i].toLowerCase(), key);
         }
-
-        return indices;
       }
+      done();
+    });
+  };
 
+  // This function will fetch the indexes from our hashtable
+  // Check if the words parameter is empty
+  // Return undefined if true
+  // Split the words to create an array
+  // If one words pass it to the getItem function
+  // If not loop through the resultant array and pass each element to the getItem function
+  HashTable.prototype.searchIndex = function(args) {
+    var searchKeys, indices = [],
+      term, key, index;
+
+    if (args !== undefined && typeof args === 'string') {
+      args = args.split(' ');
+      if (args.length === 1) {
+        return this.getItem(args);
+      } else {
+        searchKeys = args;
+      }
+    } else if (args !== undefined && typeof args === 'object') {
+      searchKeys = args;
     } else {
-      return hash.getItem(searchKeys);
+      return "Please provide a search key";
     }
 
-  } else {
-    return "Please provide a search key";
-  }
-};
-
-// hash  will be an instance of our hashtable
-// it will come with all our methods
-var url = './books.json';
-var books;
-createIndex(url);
-
-
-
+    for (key in searchKeys) {
+      term = searchKeys[key];
+      index = this.getItem(term);
+      if (index !== "Term not found") {
+        for (key in index) {
+          indices.push(index[key]);
+        }
+      } else {
+        return "No results found";
+      }
+      return indices;
+    }
+  };
+})();
+/*========================================================================================
+END OF THE HASHTABLE
+=======================================================================================*/
